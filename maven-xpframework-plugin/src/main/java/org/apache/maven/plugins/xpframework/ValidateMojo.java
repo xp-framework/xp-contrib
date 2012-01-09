6e47eaabcc9780d65bacddbfb12a00c1db0db8c3
Marius Rosoiu <marius.rosoiu@1and1.ro>
2012-01-09 16:26:06 +0200

    Replace Java-defaults project.sourceDirectory (src/main/java) and project.compileSourceRoots (src/test/java) with XP-Framework defaults (src/main/xp and src/test/xp)

--- maven-xpframework-plugin/src/main/java/org/apache/maven/plugins/xpframework/ValidateMojo.java
+++ maven-xpframework-plugin/src/main/java/org/apache/maven/plugins/xpframework/ValidateMojo.java
@@ -8,6 +8,8 @@ package org.apache.maven.plugins.xpframework;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.maven.plugin.MojoExecutionException;
 
@@ -24,6 +26,7 @@ public class ValidateMojo extends AbstractXpFrameworkMojo {
 
   /**
    * Check for XP Framework runners in PATH
+   *
    */
   public void execute() throws MojoExecutionException {
     getLog().info(LINE_SEPARATOR);
@@ -41,14 +44,76 @@ public class ValidateMojo extends AbstractXpFrameworkMojo {
     }
 
     // Alter default Maven settings
-    String xpSourceDirectory= this.basedir.getAbsolutePath() + File.separator + "src" + File.separator + "main" + File.separator + "xp";
-    this.project.addCompileSourceRoot(xpSourceDirectory);
-    getLog().debug("Add XP source directory [" + xpSourceDirectory + "] to project.compileSourceRoots");
-
-    String xpTestSourceDirectory= this.basedir.getAbsolutePath() + File.separator + "src" + File.separator + "test" + File.separator + "xp";
-    this.project.addTestCompileSourceRoot(xpTestSourceDirectory);
-    getLog().debug("Add XP test source directory [" + xpTestSourceDirectory + "] to project.testCompileSourceRoots");
-
+    this.alterSourceDirectories();
     getLog().info(LINE_SEPARATOR);
+  }
+
+  /**
+   * This will alter ${project.sourceDirectory} and ${project.testSourceDirectory} only if they have
+   * the default values set up in the Maven Super POM:
+
+   * - src/main/java
+   * - src/test/java
+   *
+   * to the following values:
+   * - src/main/xp
+   * - src/test/xp
+
+   *
+   * @return void
+   */
+  private void alterSourceDirectories() {
+
+    // Check ${project.sourceDirectory} ends with "src/main/java"
+    String oldDirectory = this.project.getBuild().getSourceDirectory();
+    String xpDirectory  = this.basedir.getAbsolutePath() + File.separator + "src" + File.separator + "main" + File.separator + "xp";
+    if (oldDirectory.endsWith("src" + File.separator + "main" + File.separator + "java")) {
+
+      // Alter ${project.sourceDirectory}
+      this.project.getBuild().setSourceDirectory(xpDirectory);
+      getLog().debug("Set ${project.sourceDirectory} to [" + xpDirectory + "]");
+
+      // Maven2 limitation: changing ${project.sourceDirectory} doesn't change ${project.compileSourceRoots}
+      List<String> newRoots= new ArrayList<String>();
+      for (String oldRoot : (List<String>)this.project.getCompileSourceRoots()) {
+        if (oldRoot.equals(oldDirectory)) {
+          newRoots.add(xpDirectory);
+        } else {
+          newRoots.add(oldRoot);
+        }
+      }
+
+      // Replace ${project.compileSourceRoots} with new list
+      this.project.getCompileSourceRoots().clear();
+      for (String newRoot : newRoots) {
+        this.project.addCompileSourceRoot(newRoot);
+      }
+    }
+
+    // Check ${project.testSourceDirectory} ends with "src/test/java"
+    oldDirectory= this.project.getBuild().getTestSourceDirectory();
+    xpDirectory= this.basedir.getAbsolutePath() + File.separator + "src" + File.separator + "test" + File.separator + "xp";
+    if (oldDirectory.endsWith("src" + File.separator + "test" + File.separator + "java")) {
+
+      // Alter ${project.testSourceDirectory}
+      this.project.getBuild().setSourceDirectory(xpDirectory);
+      getLog().debug("Set ${project.testSourceDirectory} to [" + xpDirectory + "]");
+
+      // Maven2 limitation: changing ${project.testSourceDirectory} doesn't change ${project.testCompileSourceRoots}
+      List<String> newRoots= new ArrayList<String>();
+      for (String oldRoot : (List<String>)this.project.getTestCompileSourceRoots()) {
+        if (oldRoot.equals(oldDirectory)) {
+          newRoots.add(xpDirectory);
+        } else {
+          newRoots.add(oldRoot);
+        }
+      }
+
+      // Replace ${project.testCompileSourceRoots} with new list
+      this.project.getTestCompileSourceRoots().clear();
+      for (String newRoot : newRoots) {
+        this.project.addTestCompileSourceRoot(newRoot);
+      }
+    }
   }
 }

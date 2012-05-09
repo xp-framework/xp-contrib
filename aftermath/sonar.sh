2bc3d39591a54559596f83d2a59bea7f334b53fd
Marius Rosoiu <marius.rosoiu@1and1.ro>
2012-05-09 14:30:43 +0300

    Relocated Aftermath to its own repository: https://github.com/xp-forge/aftermath

--- aftermath/sonar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xp -xar target/aftermath-1.1.xar -sr net.xp_forge.aftermath ~/workspace/pets/sonar-aftermath-plugin/src/main/resources/org/sonar/plugins/aftermath/rules.xml
-xp -xar target/aftermath-1.1.xar -sp net/xp_forge/aftermath/dogma/all-truths.xml ~/workspace/pets/sonar-aftermath-plugin/src/main/resources/org/sonar/plugins/aftermath/profiles/all-truths.xml
-xp -xar target/aftermath-1.1.xar -sp net/xp_forge/aftermath/dogma/xp-framework.xml ~/workspace/pets/sonar-aftermath-plugin/src/main/resources/org/sonar/plugins/aftermath/profiles/xp-framework.xml

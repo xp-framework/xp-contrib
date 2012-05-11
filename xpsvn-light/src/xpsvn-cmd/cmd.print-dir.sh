33dd02c7e5dc0da9b0520a5da9f6e070af8da82d
Timm Friebe <thekid@thekid.de>
2012-05-11 18:24:47 +0200

    - Obsolete, now at https://github.com/xp-forge/xpsvn

--- xpsvn-light/src/xpsvn-cmd/cmd.print-dir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# $Id: cmd.print-dir.sh 10914 2009-03-26 10:21:15Z kiesel $
-#
-
-assertHaveActiveTag
-
-echo "$(tmpTagDir)"/current-tag
--- xpsvn-light/src/xpsvn-cmd/cmd.tagupdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# $Id: cmd.tagupdate.sh 12309 2010-06-25 11:22:03Z kiesel $
-#
-
-assertHaveActiveTag
-
-cd "$(tmpTagDir)"/current-tag
-LANG=C svn update

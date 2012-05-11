47fdeccdd5a882cd7339db16320a80b21c862c41
Timm Friebe <thekid@thekid.de>
2012-05-11 18:13:21 +0200

    - Obsolete, now at https://github.com/xp-forge/sonar-aftermath-plugin

--- sonar-aftermath-plugin/sonar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm ~/workspace/tools/sonar-2.13.1/extensions/plugins/sonar-aftermath-plugin-1.0.jar
-cp target/sonar-aftermath-plugin-1.0.jar ~/workspace/tools/sonar-2.13.1/extensions/plugins/sonar-aftermath-plugin-1.0.jar
-sudo /etc/init.d/sonar restart

611c34848bc8eb38daec74bf141962fc12296e95
Oliver Hinckel <oliver.hinckel@1und1.de>
2012-07-10 15:33:45 +0200

    - add functions to JiraClient

--- jira-api/src/main/php/com/atlassian/jira/api/JiraClient.class.php
+++ jira-api/src/main/php/com/atlassian/jira/api/JiraClient.class.php
@@ -4,19 +4,55 @@
  * $Id$ 
  */
 
-  uses('com.atlassian.jira.api.protocol.JiraClientProtocolFactory');
+  uses(
+    'util.log.Traceable',
+    'com.atlassian.jira.api.protocol.JiraClientProtocolFactory',
+    'com.atlassian.jira.api.JiraClientException'
+  );
   
   /**
    * JIRA client
    *
    * @purpose  Client
    */
-  class JiraClient extends Object {
+  class JiraClient extends Object implements Traceable {
     protected
+      $cat= NULL,
       $proto= NULL;
     
     public function __construct($url) {
       $this->proto= JiraClientProtocolFactory::forURL($url);
+    }
+    
+    /**
+     * Set trace
+     * 
+     * @param util.log.LogCategory log The log category 
+     */
+    public function setTrace($cat) {
+      $this->cat= $cat;
+      
+      $this->proto->setTrace($cat);
+    }
+    
+    /**
+     * Return given issue
+     * 
+     * @param string key The issue key 
+     * @return com.atlassian.jira.api.types.JiraIssue
+     */
+    public function getIssue($key) {
+      return $this->proto->getIssue($key);
+    }
+    
+    /**
+     * Query for issues
+     * 
+     * @param com.atlassian.jira.api.query.JiraQuery query The query
+     * @return com.atlassian.jira.api.query.JiraQueryResult 
+     */
+    public function queryIssues($query) {
+      return $this->proto->queryIssues($query);
     }
   }
 

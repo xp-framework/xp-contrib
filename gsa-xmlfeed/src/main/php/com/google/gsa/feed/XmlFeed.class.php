d086e9ede8afdce2b1dab644642d7f170da3634c
Timm Friebe <thekid@thekid.de>
2012-11-28 15:12:41 +0100

    - Add records

--- gsa-xmlfeed/src/main/php/com/google/gsa/feed/XmlFeed.class.php
+++ gsa-xmlfeed/src/main/php/com/google/gsa/feed/XmlFeed.class.php
@@ -4,7 +4,10 @@
  * $Id$
  */
  
-  uses('com.google.gsa.feed.FeedType');
+  uses(
+    'com.google.gsa.feed.FeedType', 
+    'com.google.gsa.feed.Record'
+  );
 
   /**
    * Represents the XML Feed payload
@@ -12,6 +15,8 @@
    */
   class XmlFeed extends Object {
     protected $dataSource;
+    protected $feedType;
+    protected $records= array('add' => array(), 'delete' => array());
 
     /**
      * Creates a new XML Feed
@@ -40,6 +45,40 @@
      */
     public function feedType() {
       return $this->feedType;
+    }
+
+    /**
+     * Add a record
+     * 
+     * @param  string action
+     * @param  com.google.gsa.feed.Record record
+     * @return com.google.gsa.feed.Record the added record
+     * @throws lang.IllegalArgumentException when then action is unknown
+     */
+    public function addRecord($action, com·google·gsa·feed·Record $record) {
+      if (!isset($this->records[$action])) {
+        throw new IllegalArgumentException('Unknown action '.$action.', should be one of '.implode(', ', array_keys($this->records)));
+      }
+      $this->records[$action][]= $record;
+      return $record;
+    }
+
+    /**
+     * Retrieve the number of records
+     *
+     * @return  int
+     */
+    public function numRecords() {
+      return sizeof($this->records['add']) + sizeof($this->records['delete']);
+    }
+
+    /**
+     * Get all records
+     *
+     * @return  com.google.gsa.feed.Record[]
+     */
+    public function getRecords() {
+      return array_merge($this->records['add'], $this->records['delete']);
     }
   }
 ?>
--- gsa-xmlfeed/src/test/php/com/google/gsa/feed/XmlFeedTest.class.php
+++ gsa-xmlfeed/src/test/php/com/google/gsa/feed/XmlFeedTest.class.php
@@ -4,7 +4,11 @@
  * $Id$
  */
  
-  uses('unittest.TestCase', 'com.google.gsa.feed.XmlFeed');
+  uses(
+    'unittest.TestCase', 
+    'com.google.gsa.feed.XmlFeed',
+    'com.google.gsa.feed.UrlRecord'
+  );
  
   /**
    * Tests XML Feed implementation
@@ -28,6 +32,38 @@
     #[@test]
     public function feed_type() {
       $this->assertEquals(FeedType::$FULL, create(new XmlFeed('test', FeedType::$FULL))->feedType());
+    }
+
+    /**
+     * Test addRecord
+     *
+     */
+    #[@test]
+    public function add_record() {
+      $feed= new XmlFeed('test', FeedType::$FULL);
+      $feed->addRecord('add', new UrlRecord('http://localhost'));
+      $this->assertEquals(1, $feed->numRecords());
+    }
+
+    /**
+     * Test addRecord
+     *
+     */
+    #[@test]
+    public function delete_record() {
+      $feed= new XmlFeed('test', FeedType::$FULL);
+      $feed->addRecord('delete', new UrlRecord('http://localhost'));
+      $this->assertEquals(1, $feed->numRecords());
+    }
+
+    /**
+     * Test addRecord
+     *
+     */
+    #[@test, @expect('lang.IllegalArgumentException')]
+    public function add_record_with_unknown_action() {
+      $feed= new XmlFeed('test', FeedType::$FULL);
+      $feed->addRecord('@@unknown@@', new UrlRecord('http://localhost'));
     }
   }
 ?>

265b52eddba5835e52759966a2591d1e00179d94
Timm Friebe <thekid@thekid.de>
2012-11-29 16:36:49 +0100

    - Extend testing to payload

--- gsa-xmlfeed/src/test/php/com/google/gsa/feed/FeedConnectionTest.class.php
+++ gsa-xmlfeed/src/test/php/com/google/gsa/feed/FeedConnectionTest.class.php
@@ -6,7 +6,8 @@
  
   uses(
     'unittest.TestCase', 
-    'com.google.gsa.feed.FeedConnection'
+    'com.google.gsa.feed.FeedConnection',
+    'com.google.gsa.feed.XmlFeed'
   );
  
   /**
@@ -38,6 +39,41 @@
       $this->assertEquals(
         new URL($url), 
         create(new FeedConnection(new HttpConnection($url)))->getConnection()->getUrl()
+      );
+    }
+
+    /**
+     * Assertion helper 
+     *
+     * @param  string expected
+     * @param  string actual
+     * @throws unittest.AssertionFailedError
+     */
+    protected function assertXmlEquals($expected, $actual) {
+      $this->assertEquals(
+        preg_replace('/[\s\t\r\n]+/', '', $expected),
+        preg_replace('/[\s\t\r\n]+/', '', $actual)
+      );
+    }
+
+    /**
+     * Test constructor
+     *
+     */
+    #[@test]
+    public function feed_payload() {
+      $this->assertXmlEquals(
+        '<?xml version="1.0" encoding="ISO-8859-1"?>
+        <!DOCTYPE gsafeed PUBLIC "-//Google//DTD GSA Feeds//EN" "">
+        <gsafeed>
+          <header>
+            <datasource>test</datasource>
+            <feedtype>full</feedtype>
+          </header>
+          <group/>
+        </gsafeed>
+        ',
+        create(new FeedConnection('http://localhost:19900/xmlfeed'))->payload(new XmlFeed('test', FeedType::$FULL))
       );
     }
   }
